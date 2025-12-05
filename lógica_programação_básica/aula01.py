a9a0a7c51292abb6902ed9da8730d0956c87d8c5
Fernando Sales <nandolassance@icloud.com>
2025-12-05 20:58:41 -0300

    Adiciona o conceito de DocStrings, para comentários de multiplas linhas.

--- lógica_programação_básica/aula01.py
+++ lógica_programação_básica/aula01.py
@@ -1,2 +1,12 @@
-#Este é um comentário de uma linha em Python
+#Este é um comentário de linha única em Python.
 print("Olá, mundo!") # Imprime a mensagem na tela.
+
+""""
+DocString, é usado para comentar múltiplas linhas. 
+Pode ser usado com 'aspas duplas'ou 'aspas simples'.
+
+"""
+
+''''
+DocString, usado com aspas simples para comentar múltiplas linhas.
+'''
