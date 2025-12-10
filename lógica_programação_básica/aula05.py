d73c84747cf374df11ae3bee3d9a14993c0ec841
Fernando Sales <nandolassance@icloud.com>
2025-12-09 21:57:22 -0300

    Adiciona o arquivo da aula 5 - boleanos

---
+++ lógica_programação_básica/aula05.py
@@ -0,0 +1,7 @@
+#Aula sobre 'booleanos' ou seja 'true' ou 'false'
+
+print(10 == "Verdadeiro") #Imprime 'false' pois sáo tipos diferentes (int e str) 
+print(10 == 10) #Imprime 'true' pois sáo do mesmo tipo (int) e mesmo valor 
+
+print(type(10 == 10)) #Imprime o tipo do dado '10 == 10' que é 'bool' (booleano) (true)
+print(type(10 == "Verdadeiro")) #Imprime o tipo do dado '10 == "Verdadeiro"' que é 'bool' (booleano) (false)  
