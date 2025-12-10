4c93aafa76c12b45f7bb2b74235beee92b1233f0
Fernando Sales <nandolassance@icloud.com>
2025-12-09 21:21:41 -0300

    Adiciona o arquivo aula 4 oara tipos de números (int) e (float)

---
+++ lógica_programação_básica/aula04.py
@@ -0,0 +1,13 @@
+#função 'type' mostra o tipo do dado passado como argumento e númertos do tipo 'ínt'
+
+print(1) #imprime o valor '1'no console
+# print(2) #imprime o valor '2'no console
+# print(3) #imprime o valor '3'no console 
+
+# comamand + / comenta a linha selecionada no VS Code
+
+print(1.75) #Imprime o valor '1.75'no console, do tipo ponto flutuante (float)
+print(-1.75) #imprime o valor '-1.75'no console, do tipo ponto flutuante (float) (negativo)
+print(type(1)) #imprime o tipo do dado '1' no console que é 'int' (inteiro)
+print(type(722.50)) #imprime o tipo do dado '722.50'no console que é 'float' (ponto flutuante)
+print(type(-722.50)) #imprime o tipo de dado '-722.50'no console que é 'float' (ponto flutuante) (negativo)
