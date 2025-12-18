2873aacee35385cd5f40b724ae2b01dedcd87b9f
Fernando Sales <nandolassance@icloud.com>
2025-12-18 19:15:50 -0300

    Adiciona Aula 07, variaveisá

---
+++ lógica_programação_básica/aula07.py
@@ -0,0 +1,18 @@
+#Variáveis 
+
+nome = "Fernando"
+nome_completo = "Fernando Sales"
+idade = 36 
+maior_de_idade = idade >= 18
+altura_metros = 1.75 
+
+print("Nome:", nome, "atualmente tenho:", idade, "anos de idade.", "Minha altura é:", altura_metros)
+print("Nome completo:", nome_completo)
+print("Fernando é maior de idade?", maior_de_idade)
+
+int_um = int('1') #Converte a string '1' para o inteiro 1
+print(int_um)
+soma_dois_mais_dois = 2 + 2
+print("A soma de 2 + 2 é igual á:", soma_dois_mais_dois)
+double_um = type(float(int_um)) 
+print("O tipo de double_um é:", double_um)
