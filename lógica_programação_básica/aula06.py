0a803e2d06609040bf94113f28c06ecc8252423d
Fernando Sales <nandolassance@icloud.com>
2025-12-18 18:35:52 -0300

    Adiciona a Aula 06

---
+++ lógica_programação_básica/aula06.py
@@ -0,0 +1,10 @@
+#Coerção de tipos em Python
+
+print(1 + 722)
+print ("a" + "b") 
+
+print("1", type("1"))
+print(int("1"), type(int("1"))) #Converte o tipo string "1"para tipo inteiro 1 
+print(int("100") + 50) #Converte o tipo string "100" para tipo inteiro 100 e soma com 50    
+print("Fernando, atualmente você mora no apartamento",  + (str(151)), + "A")
+
