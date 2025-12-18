6c64b3348c781fee0cd883a7219bdb7b875c3ae2
Fernando Sales <nandolassance@icloud.com>
2025-12-18 18:39:09 -0300

    Corrige aula 6

--- lógica_programação_básica/aula06.py
+++ lógica_programação_básica/aula06.py
@@ -6,5 +6,4 @@ print ("a" + "b")
 print("1", type("1"))
 print(int("1"), type(int("1"))) #Converte o tipo string "1"para tipo inteiro 1 
 print(int("100") + 50) #Converte o tipo string "100" para tipo inteiro 100 e soma com 50    
-print("Fernando, atualmente você mora no apartamento",  + (str(151)), + "A")
-
+print("Fernando, atualmente você mora no apartamento", str(151), "A", sep=" ")
