9ddb77c042af21dbc2b9daf642bb41f7ce802283
Fernando Sales <nandolassance@icloud.com>
2025-12-20 22:21:46 -0300

    Adiciona Aula 08 - Exercicios de Tipos e Variaveis

---
+++ lógica_programação_básica/aula08.py
@@ -0,0 +1,14 @@
+#Exerçicio dos tipos de dados e Variaveis 
+
+nome = 'Fernando'
+sobrenome = 'Sales'
+idade = 33
+altura = 1.70
+
+print("Nome:", nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Altura:', altura)
+print('É maior de idade?', idade >= 18)
+
+print('Olá, o meu nome é ', nome, sobrenome, ' tenho atualmente: ', idade, ' anos de idade e minha altura é: ', altura, sep= ' ')
