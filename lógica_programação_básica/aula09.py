45d4755b5b2469771bb5cd046f95f31008792742
Fernando Sales <nandolassance@icloud.com>
2025-12-22 22:32:03 -0300

    Adiciona Aula 09 - Operadores Aritméticos

---
+++ lógica_programação_básica/aula09.py
@@ -0,0 +1,22 @@
+#Operadores Aritmeticos
+
+adicao = 10 + 10 #Soma os dois números
+print("Resultado da Adição:", adicao)
+
+subtracao = 722 - 22  #Subtra o segundo número do primeiro
+print("Resultado da Subtração:", subtracao)
+
+multiplicacao = 10 * 100 #Multiplica os dois números
+print("Resultado da Multiplicação:", multiplicacao)
+
+divisao = 1000 / 10 #Divide o primeiro número pelo segundo
+print("Resultado da Divisão:", divisao)
+
+divisao_inteira = 1000 // 7 #Divide o primeiro número pelo segundo e retorna a parte inteira
+print("Resultado da Divisão Inteira:", divisao_inteira)
+
+operador_modulo = 50 % 3 #Retorna o resto da divisão do primeiro número pelo segundo
+print("Resultado do Módulo (Resto da Divisão):", operador_modulo)
+
+exponenciacao = 2 ** 10 #Eleva o primeiro número à potência do segundo
+print("Resultado da Exponenciação:", exponenciacao)
