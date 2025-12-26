ef98226d4320b7cb3f3c1d969b4d739cf93b0898
Fernando Sales <nandolassance@icloud.com>
2025-12-26 02:25:44 -0300

    Adiciona Aula 10 - Operaçoes especiais com + e * e letras

---
+++ lógica_programação_básica/aula10.py
@@ -0,0 +1,8 @@
+letras_concatenadas = "Fernando" + " " + "Sales"
+print(letras_concatenadas)
+
+letras_repetidas = "A" * 20
+print(letras_repetidas)
+
+repeticao_nome = "Fernando" * 5
+print(repeticao_nome)
