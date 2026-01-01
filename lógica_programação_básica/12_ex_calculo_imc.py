be994c0ac632ef0268d0875ce0303370a909382c
Fernando Sales <nandolassance@icloud.com>
2026-01-01 17:20:25 -0300

    Adiciona a Aula 12 - com exercício básisco de IMC

---
+++ lógica_programação_básica/12_ex_calculo_imc.py
@@ -0,0 +1,9 @@
+# ... São 'Ellipsis' (reticências) usadas para indicar que um bloco de código está incompleto
+
+nome_completo = 'Fernando Sales'
+altura_metros = 1.70
+peso_kg = 80
+imc = peso_kg / (altura_metros * altura_metros)
+
+print(f"O Analista {nome_completo} possui {altura_metros}, de altura")
+print(f"e pesa {peso_kg} kg, portando seu IMC é de {imc}")
