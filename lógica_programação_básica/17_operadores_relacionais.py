c03a73b971ebce6fe1854c97c27c60b534bc4af8
Fernando Sales <nandolassance@icloud.com>
2026-01-07 21:25:45 -0300

    Adiciona o arquivo 17 com a aula sobre operadores relacionais

---
+++ lógica_programação_básica/17_operadores_relacionais.py
@@ -0,0 +1,36 @@
+"""
+Operadores Relacionais em Python - São usados para se comparar valores.
+Eles retornam um valor booleano: True (verdadeiro) ou False (falso)
+Exemplos de Operadores Relacionais:
+==  : Igual a
+!=  : Diferente de
+>   : Maior que
+<   : Menor que
+>=  : Maior ou igual a
+<= : Menor ou igual a
+
+"""
+
+comparador_igual = 2 == 2  # True
+print(f"O resultado da variável 'comparador_igual' é: {comparador_igual}")
+
+comparador_diferente = 2 != 3  # True
+print(f"O resultado da variável 'comparador_diferente' é: {comparador_diferente}")
+
+comparador_maior = 5 > 2  # True
+print(f"O resultado da variável 'comparador_maior' é: {comparador_maior}")
+
+comparador_menor = 10 < 700  # True
+print(f"O resultado da variável 'comparador_menor' é: {comparador_menor}")
+
+comparador_maior_igual = 12 >= 12  # True
+print(f"O resultado da variável 'comparador_maior_igual' é: {comparador_maior_igual}")
+
+comparador_menor_igual = 12 <= 5  # False
+print(f"O resultado da variável 'comparador_menor_igual' é: {comparador_menor_igual}")
+
+"""
+O comando para usar o shell interativo do Python é: 'python -i nome_arquivo.py'
+Dessa forma, após a execução do arquivo, você pode testar os operadores relacionais 
+diretamente no shell.
+"""
