5def3643f0f225df8f038538530115cf369236e3
Fernando Sales <nandolassance@icloud.com>
2026-01-08 22:16:51 -0300

    Ajusta mensages de print da aula 18

--- lógica_programação_básica/18_ex_condicionais_if_ex.py
+++ lógica_programação_básica/18_ex_condicionais_if_ex.py
@@ -13,10 +13,10 @@ print(" --- CONTINUANDO O PROGRAMA --- ")
 print(" ---- ANALISANDO OS VALORES E FAZENDO AS COMPARAÇÕES ---- ") 
 
 if primeiro_valor > segundo_valor:
-    print(f"O 1º valor registrado na variável 'primeiro_valor' = {primeiro_valor} é maior que o 2º valor registrado na variável 'segundo_valor' = {segundo_valor}.")
+    print(f"O 1º valor={primeiro_valor} é maior que o 2º valor={segundo_valor}")
 elif primeiro_valor == segundo_valor:
-    print(f"O 1º valor registrado na variável 'primeiro_valor' = {primeiro_valor} é igual ao 2º valor da variável 'segundo_valor' = {segundo_valor}.")
+    print(f"O 1º valor={primeiro_valor} é igual ao 2º valor={segundo_valor}")
 else:
-    print(f"O 2º valor registrado na variável 'segundo_valor' = {segundo_valor} é maior que o 1º valor registrado na variável 'primeiro_valor' = {primeiro_valor}.")
+    print(f"O 2º valor={segundo_valor} é maior que o 1º valor={primeiro_valor}")
 
 print(" ---- PROGRAMA FINALIZADO - ATÉ A PRÓXIMA! ---- ")
